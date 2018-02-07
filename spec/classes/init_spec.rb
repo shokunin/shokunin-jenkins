d5cdda3cf07ef1ac6da94bae201dbc6d5c19f56a
Christian Mague <cmague@konghq.com>
2018-02-07 15:08:23 -0800

    fix(rspec) add features and facter facts

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 describe 'jenkins' do
+  let(:facts) do
+    {
+      'osfamily' => 'Debian',
+      'os' => {
+        'name'    => 'Ubuntu',
+        'family'  => 'Debian',
+        'release' => { 'major' => '16.04', 'full' => '16.04' },
+        'lsb'     =>
+                              {
+                                'distcodename'    => 'xenial',
+                                'distid'          => 'Ubuntu',
+                                'distdescription' => 'Ubuntu 16.04.3 LTS',
+                                'distrelease'     => '16.04',
+                                'majdistrelease'  => '16.04'
+                              }
+      }
+    }
+  end
   context 'with default values for all parameters' do
     it { should contain_class('jenkins') }
   end
