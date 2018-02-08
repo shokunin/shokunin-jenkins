ae674b29749e480deab9cdee45badb8df261efa9
Christian Mague <cmague@konghq.com>
2018-02-08 11:18:14 -0800

    add filesystem mount

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -3,6 +3,7 @@
 class jenkins::volume {
   unless $jenkins::disk_device == '' {
     include ::lvm
+    include ::stdlib
 
     physical_volume { $jenkins::disk_device:
       ensure => present,
@@ -18,6 +19,11 @@ class jenkins::volume {
       ensure => directory,
       owner  => jenkins,
       group  =>  jenkins,
+    }
+
+    file_line { 'jenkins_mount':
+      path => '/etc/fstab',
+      line => "${jenkins::disk_device} ${jenkins::disk_mount} ext4  defaults,nofail 0  2"
     }
 
   }
