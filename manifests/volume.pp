97fe475661a7ff1d508c2231ab24c3d53cd351fe
Christian Mague <cmague@konghq.com>
2018-02-08 11:19:53 -0800

    volume fixes

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -5,14 +5,9 @@ class jenkins::volume {
     include ::lvm
     include ::stdlib
 
-    physical_volume { $jenkins::disk_device:
-      ensure => present,
-    }
-
     filesystem { $jenkins::disk_device:
       ensure  => present,
       fs_type => 'ext4',
-      require => Physical_Volume[$jenkins::disk_device],
     }
 
     file { $jenkins::disk_mount:
