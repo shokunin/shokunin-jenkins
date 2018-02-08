b98e9fddf3fa19d5e9997ef13f1eaa78d47e251c
Christian Mague <cmague@konghq.com>
2018-02-08 12:24:16 -0800

    more ordering

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -2,4 +2,12 @@
 # ===========================
 class jenkins::configure {
 
+  file { '/var/lib/jenkins/.gitconfig':
+    ensure  => present,
+    owner   => jenkins,
+    group   => jenkins,
+    mode    => '0644',
+    content => template('jenkins/gitconfig.erb'),
+  }
+
 }
--- manifests/user.pp
+++ manifests/user.pp
@@ -13,12 +13,4 @@ class jenkins::user {
     home    => '/var/lib/jenkins',
   }
 
-  file { '/var/lib/jenkins/.gitconfig':
-    ensure  => present,
-    owner   => jenkins,
-    group   => jenkins,
-    mode    => '0644',
-    content => template('jenkins/gitconfig.erb'),
-  }
-
 }
