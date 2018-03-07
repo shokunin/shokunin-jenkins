e17c298ba8f9cf19f7c486ffc2672a2173674913
Christian Mague <cmague@konghq.com>
2018-03-07 14:01:37 -0800

    drop in a service file

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -35,10 +35,20 @@ class jenkins::configure {
   }
 
 
-  if $::jenkins_has_consul { 
-    notify { 'DTM: Enabled': }
-  } else {
-    notify { 'DTM: Enabled': }
+  if $::jenkins_has_consul {
+    file { '/opt/consul/etc/jenkins.json':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      content => '{"service":{ "name": "jenkins", "port": 8080, "checks": [{ "script": "/usr/bin/curl -o /dev/null -sf localhost:8080/login", "interval": "10s"}]}}',
+      notify  =>  Exec['jenkins_reload_consul'],
+    }
+
+    exec { 'jenkins_reload_consul':
+      command     => '/usr/local/bin/supervisorctl restart consul',
+      refreshonly => true,
+    }
+
   }
 
 }
