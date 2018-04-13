021f04bec12afd472f2bb7ca571f79720131e72f
Christian Mague <cmague@konghq.com>
2018-04-13 11:19:02 -0700

    setup utilities

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,16 @@
 # Class: jenkins
 # ===========================
 class jenkins (
-  Boolean $manage_apt_repo = true,
-  Array   $prereqs         = ['openjdk-8-jre-headless'],
-  String  $disk_device     = '',
-  String  $disk_mount      = '/var/lib/jenkins',
-  String  $packer_version  = '1.1.3',
-  String  $ruby_version    = '2.3.3',
-  String  $repo_url        = 'http://pkg.jenkins-ci.org/debian',
-  String  $repo_key_id     = '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-  String  $repo_key_source = 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+  Boolean $manage_apt_repo  = true,
+  Boolean $manage_utilities = false,
+  Array   $prereqs          = ['openjdk-8-jre-headless'],
+  String  $disk_device      = '',
+  String  $disk_mount       = '/var/lib/jenkins',
+  String  $packer_version   = '1.1.3',
+  String  $ruby_version     = '2.3.3',
+  String  $repo_url         = 'http://pkg.jenkins-ci.org/debian',
+  String  $repo_key_id      = '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+  String  $repo_key_source  = 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
 ){
 
   contain jenkins::apt_repo
@@ -19,6 +20,10 @@ class jenkins (
   contain jenkins::volume
   contain jenkins::user
   contain jenkins::postinstall
+
+  if $::jenkins::manage_utilities {
+    include ::jenkins::utilities
+  }
 
   Class['::jenkins::apt_repo']
   -> Class['::jenkins::user']
---
+++ manifests/utlities.pp
@@ -0,0 +1,14 @@
+# Class: jenkins::utlities
+# ===========================
+class jenkins::utlities {
+
+  # source for this can be found at https://raw.githubusercontent.com/shokunin/ops_scripts/master/elastic_search/logstash_cleanup.go
+  file { '/usr/local/bin/logstash_cleanup':
+    ensure => prent,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/jenkins/logstash_cleanup',
+  }
+
+}
