41d2bf3cceca6a1964f85b9ad59a3754180d6079
Christian Mague <cmague@konghq.com>
2018-04-13 11:25:46 -0700

    fix spelling errors

--- manifests/utilities.pp
+++ manifests/utilities.pp
@@ -1,6 +1,6 @@
 # Class: jenkins::utlities
 # ===========================
-class jenkins::utlities {
+class jenkins::utilities {
 
   # source for this can be found at https://raw.githubusercontent.com/shokunin/ops_scripts/master/elastic_search/logstash_cleanup.go
   file { '/usr/local/bin/logstash_cleanup':
