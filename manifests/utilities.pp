790079622847c675c16ee7f2e565503f4b4bf258
Christian Mague <cmague@konghq.com>
2018-04-13 11:28:43 -0700

    spelling is hard

--- manifests/utilities.pp
+++ manifests/utilities.pp
@@ -4,7 +4,7 @@ class jenkins::utilities {
 
   # source for this can be found at https://raw.githubusercontent.com/shokunin/ops_scripts/master/elastic_search/logstash_cleanup.go
   file { '/usr/local/bin/logstash_cleanup':
-    ensure => prent,
+    ensure => file,
     owner  => root,
     group  => root,
     mode   => '0755',
